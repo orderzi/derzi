1d3b7f345d0985753ff84b63b0458492761ab699
root <root@ip-172-31-38-130.us-east-2.compute.internal>
2019-12-23 16:41:07 +0000

    third change

---
+++ project.py
@@ -0,0 +1,37 @@
+from netmiko import ConnectHandler
+
+username = 'ubuntu'
+#password = 'password'
+host='ec2-18-222-28-87.us-east-2.compute.amazonaws.com'
+
+def Connect_to_server(username,host):
+    Connected = False
+    if Connected == False:
+        try:
+            server = ConnectHandler(device_type='linux',username=username,host=host,key_file='bla.pem',timeout=10)
+            return server
+        except:
+            print('server is down')
+            return False
+
+
+
+
+def show_files(server):
+    show_dir = server.send_command('ls -l')
+    return show_dir
+
+
+if __name__ == '__main__':
+    print('Connecting to server..')
+    server = Connect_to_server(username,host)
+    if server:
+        print('finding files..')
+        file = show_files(server)
+        print(file)
+    if not server:
+        print('server is down , files not found.')
+
+
+
+
